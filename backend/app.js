28208d0ba4a92f17202084c8f316f06164465ee4
Himanshu First <himanshunaidu12161997@gmail.com>
2021-09-11 14:13:56 +0530

    Add routes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const homeRouter = require('./routes/home-routes');
+const contactRouter = require('./routes/contact-routes');
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', homeRouter);
+app.use('/contact', contactRouter);
 
 app.use((req, res, next) => {
     res.status(404).json({ status: "Page Not Found" });
