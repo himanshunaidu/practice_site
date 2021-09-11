0c7414d0db79e350be8fb37f143448721be4272a
Himanshu First <himanshunaidu12161997@gmail.com>
2021-09-11 19:10:00 +0530

    Contact Page

---
+++ frontend/src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div>September 11, 2021</div>
+      <h2>Name: </h2>
+      <label>Himanshu Naidu</label>
+    </div>
+  );
+}
+
+export default Contact;
