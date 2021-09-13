b154e9b9eb006a2e3beed6f3c0cb5cc31117d5d9
Himanshu First <himanshunaidu12161997@gmail.com>
2021-09-13 16:09:18 +0530

    Frontend modularity

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,14 +1,16 @@
 import React from "react";
+
 import "./Contact.css";
+import Card from "../UI/Card/Card";
 
 function Contact(props) {
   return (
-    <div className="contact-item">
-      <div className="contact-item__description">
-        <h2>{props.field}</h2>
-        <div className="contact-item__val">{props.value}</div>
-      </div>
-    </div>
+    <Card className="contact-item">
+      {/* <div className="contact-item__description"> */}
+      <h2>{props.field}</h2>
+      <div className="contact-item__val">{props.value}</div>
+      {/* </div> */}
+    </Card>
   );
 }
 
