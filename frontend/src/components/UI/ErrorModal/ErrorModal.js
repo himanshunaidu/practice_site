bb75fc6411acb3500551cc27a18cdd0c12e5db38
Himanshu First <himanshunaidu12161997@gmail.com>
2021-09-19 14:16:12 +0530

    Modal creation

---
+++ frontend/src/components/UI/ErrorModal/ErrorModal.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const ErrorModal = (props) => {
+  return (
+    <Card>
+      <header>
+        <h2></h2>
+      </header>
+      <div>
+        <p></p>
+      </div>
+      <footer></footer>
+    </Card>
+  );
+};
+
+export default ErrorModal;
