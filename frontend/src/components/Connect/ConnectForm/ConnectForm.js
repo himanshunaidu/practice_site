a536fbb622b0b1ab99957089046fd61dd818a747
Himanshu First <himanshunaidu12161997@gmail.com>
2021-09-19 14:54:30 +0530

    Use modal

--- frontend/src/components/Connect/ConnectForm/ConnectForm.js
+++ frontend/src/components/Connect/ConnectForm/ConnectForm.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import ConnectMessage from "../../../models/Connect/connect";
 
+import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 import Button from "../../UI/Button/Button";
 import styles from "./ConnectForm.module.css";
 
+const initialErrorState = {
+  show: false,
+  title: "",
+  message: "",
+};
+
 const ConnectForm = (props) => {
+  //State Variables
   const [title, setTitle] = useState("");
   const [titleValid, setTitleValid] = useState(true);
 
@@ -17,8 +25,10 @@ const ConnectForm = (props) => {
   const [message, setMessage] = useState("");
   const [messageValid, setMessageValid] = useState(true);
 
+  const [errorState, setErrorState] = useState(initialErrorState);
   const [updateCount, setUpdateCount] = useState(0);
 
+  //Enum
   const connectFields = {
     title: 1,
     name: 2,
@@ -26,6 +36,7 @@ const ConnectForm = (props) => {
     message: 4,
   };
 
+  //Handlers
   const fieldChangeHandler = (field, event) => {
     let value = event.target.value.trim();
 
@@ -86,64 +97,83 @@ const ConnectForm = (props) => {
 
     if (sendFlag) {
       props.sendMessage(title, name, mobile, message);
+    } else {
+      setErrorState({
+        show: true,
+        title: "Invalid Input",
+        message: "Please Enter Valid Input",
+      });
     }
   };
 
+  const errorConfirmHandler = (event) => {
+    setErrorState(initialErrorState);
+  };
+
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className={styles.connect__controls}>
-        <div className={styles.connect__control}>
-          <label>Title</label>
-          <input
-            className={titleValid ? "" : styles.invalid}
-            type="text"
-            value={title}
-            onChange={(e) => {
-              fieldChangeHandler(connectFields.title, e);
-            }}
-          />
+    <React.Fragment>
+      {errorState.show ? (
+        <ErrorModal
+          title={"Invalid Input"}
+          message={"Please Enter valid input"}
+          onConfirm={errorConfirmHandler}
+        ></ErrorModal>
+      ) : null}
+      <form onSubmit={formSubmitHandler}>
+        <div className={styles.connect__controls}>
+          <div className={styles.connect__control}>
+            <label>Title</label>
+            <input
+              className={titleValid ? "" : styles.invalid}
+              type="text"
+              value={title}
+              onChange={(e) => {
+                fieldChangeHandler(connectFields.title, e);
+              }}
+            />
+          </div>
+          <div className={styles.connect__control}>
+            <label>Name</label>
+            <input
+              className={nameValid ? "" : styles.invalid}
+              type="text"
+              value={name}
+              onChange={(e) => {
+                fieldChangeHandler(connectFields.name, e);
+              }}
+            />
+          </div>
+          <div className={styles.connect__control}>
+            <label>Mobile</label>
+            <input
+              className={mobileValid ? "" : styles.invalid}
+              type="number"
+              value={mobile}
+              onChange={(e) => {
+                fieldChangeHandler(connectFields.mobile, e);
+              }}
+            />
+          </div>
+          <div className={styles.connect__control}>
+            <label>Message</label>
+            <textarea
+              className={messageValid ? "" : styles.invalid}
+              type="number"
+              value={message}
+              rows={4}
+              cols={50}
+              onChange={(e) => {
+                fieldChangeHandler(connectFields.message, e);
+              }}
+            />
+          </div>
         </div>
-        <div className={styles.connect__control}>
-          <label>Name</label>
-          <input
-            className={nameValid ? "" : styles.invalid}
-            type="text"
-            value={name}
-            onChange={(e) => {
-              fieldChangeHandler(connectFields.name, e);
-            }}
-          />
+        <div className={styles.connect__actions}>
+          {/* <button type="submit">Send Message</button> */}
+          <Button type="submit">Send Message</Button>
         </div>
-        <div className={styles.connect__control}>
-          <label>Mobile</label>
-          <input
-            className={mobileValid ? "" : styles.invalid}
-            type="number"
-            value={mobile}
-            onChange={(e) => {
-              fieldChangeHandler(connectFields.mobile, e);
-            }}
-          />
-        </div>
-        <div className={styles.connect__control}>
-          <label>Message</label>
-          <textarea
-            className={messageValid ? "" : styles.invalid}
-            type="number"
-            value={message}
-            rows={4}
-            cols={50}
-            onChange={(e) => {
-              fieldChangeHandler(connectFields.message, e);
-            }}
-          />
-        </div>
-      </div>
-      <div className={styles.connect__actions}>
-        {/* <button type="submit">Send Message</button> */}
-        <Button type="submit">Send Message</Button>
-      </div>
-    </form>
+      </form>
+    </React.Fragment>
   );
 };
 
--- frontend/src/components/UI/ErrorModal/ErrorModal.js
+++ frontend/src/components/UI/ErrorModal/ErrorModal.js
@@ -6,15 +6,20 @@ import styles from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={styles.actions}></footer>
-    </Card>
+    <React.Fragment>
+      <div className={styles.backdrop} onClick={props.onConfirm}></div>
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </React.Fragment>
   );
 };
 
