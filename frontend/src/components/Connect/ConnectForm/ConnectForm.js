723d67982fb182839f8d57f024e5bff58d9f7856
Himanshu First <himanshunaidu12161997@gmail.com>
2021-09-22 20:48:32 +0530

    Usage of useReducer

--- frontend/src/components/Connect/ConnectForm/ConnectForm.js
+++ frontend/src/components/Connect/ConnectForm/ConnectForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import ConnectMessage from "../../../models/Connect/connect";
 
 import ErrorModal from "../../UI/ErrorModal/ErrorModal";
@@ -11,10 +11,32 @@ const initialErrorState = {
   message: "",
 };
 
+//Reducer for the title state. Can be defined outside the component as it does not depend on the component
+//Enum
+const titleActions = {
+  userInput: "USER_INPUT",
+  default: "DEFAULT",
+  mobile: 3,
+  message: 4,
+};
+
+const titleReducer = (state, action) => {
+  if (action.type === titleActions.userInput) {
+    return { value: action.val, isValid: action.val.trim().length !== 0 };
+  }
+  return { value: "", isValid: false };
+};
+
 const ConnectForm = (props) => {
   //State Variables
-  const [title, setTitle] = useState("");
-  const [titleValid, setTitleValid] = useState(false);
+
+  //useReducer in this case
+  // const [title, setTitle] = useState("");
+  // const [titleValid, setTitleValid] = useState(false);
+  const [titleState, dispatchTitle] = useReducer(titleReducer, {
+    value: "",
+    isValid: false,
+  });
 
   const [name, setName] = useState("");
   const [nameValid, setNameValid] = useState(false);
@@ -29,42 +51,41 @@ const ConnectForm = (props) => {
 
   const [errorState, setErrorState] = useState(initialErrorState);
 
-  //Enum (Not useful anymore)
-  const connectFields = {
-    title: 1,
-    name: 2,
-    mobile: 3,
-    message: 4,
-  };
-
   //Validation useEffect
   useEffect(() => {
     const debouncer = setTimeout(() => {
-      let titleValidLoc = title.trim().length === 0;
+      // let titleValidLoc = titleState.value.trim().length === 0;
       let nameValidLoc = name.trim().length === 0;
       let mobileValidLoc = Math.floor(Math.log10(mobile)) + 1 !== 10;
       let messageValidLoc = message.trim().length === 0;
 
-      setTitleValid(!titleValidLoc);
+      // setTitleValid(!titleValidLoc);
       setNameValid(!nameValidLoc);
       setMobileValid(!mobileValidLoc);
       setMessageValid(!messageValidLoc);
+      console.log(titleState);
 
       setFormValid(
-        !(titleValidLoc || nameValidLoc || mobileValidLoc || messageValidLoc)
+        !(
+          !titleState.isValid ||
+          nameValidLoc ||
+          mobileValidLoc ||
+          messageValidLoc
+        )
       );
     }, 500);
     return () => {
       clearTimeout(debouncer);
     };
-  }, [title, name, mobile, message]);
+  }, [titleState.isValid, name, mobile, message]);
 
   const formSubmitHandler = (event) => {
     event.preventDefault(); //This prevents the form from re-loading the page
     if (formValid) {
-      props.sendMessage(title, name, mobile, message);
+      props.sendMessage(titleState.value, name, mobile, message);
       //Reset
-      setTitle("");
+      // setTitle("");
+      dispatchTitle({ type: titleActions.default });
       setName("");
       setMobile(0);
       setMessage("");
@@ -96,11 +117,14 @@ const ConnectForm = (props) => {
           <div className={styles.connect__control}>
             <label>Title</label>
             <input
-              className={titleValid ? "" : styles.invalid}
+              className={titleState.isValid ? "" : styles.invalid}
               type="text"
-              value={title}
+              value={titleState.value}
               onChange={(e) => {
-                setTitle(e.target.value);
+                dispatchTitle({
+                  type: titleActions.userInput,
+                  val: e.target.value,
+                });
               }}
             />
           </div>
